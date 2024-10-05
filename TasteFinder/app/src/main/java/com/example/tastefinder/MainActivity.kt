02a4ed0544462e6e216014b1f1fa748b17b8d17b
Sara Ashraf <saraashraf1822004@gmail.com>
2024-10-05 03:56:56 +0300

    Splash Screen and registration

---
+++ TasteFinder/app/src/main/java/com/example/tastefinder/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tastefinder
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        Thread.sleep(3000)
+        installSplashScreen()
+        setContentView(R.layout.activity_main)
+
+    }
+}
