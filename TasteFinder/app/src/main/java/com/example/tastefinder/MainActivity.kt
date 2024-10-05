3aa803b9d66680b2383594f7f7f1e102dfab795d
Sara Ashraf <saraashraf1822004@gmail.com>
2024-10-06 01:51:12 +0300

    Sara's update

--- TasteFinder/app/src/main/java/com/example/tastefinder/MainActivity.kt
+++ TasteFinder/app/src/main/java/com/example/tastefinder/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tastefinder
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -8,12 +12,24 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        Thread.sleep(3000)
-        installSplashScreen()
         setContentView(R.layout.activity_main)
 
+        // Register
+        val register = findViewById<TextView>(R.id.registerTxt)
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        // LogIn
+        val logIn = findViewById<TextView>(R.id.loginTxt)
+        register.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
